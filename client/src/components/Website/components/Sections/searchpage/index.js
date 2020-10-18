ae120d62556b741f328448b02e9edc4e71c5f8a2
Deep Golani <54791570+deepgolani4@users.noreply.github.com>
2020-10-18 18:21:44 +0530

    Update index.js

--- client/src/components/Website/components/Sections/searchpage/index.js
+++ client/src/components/Website/components/Sections/searchpage/index.js
@@ -10,28 +10,32 @@ import Page from './Page';
 import Loading from '../../../Loading';
 import { useLocation } from 'react-router-dom';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 var data = [
   {
-    name: "all marty",
+    name: "allmart",
     products: [
       {
-        src:
+        image_link:
           'https://images-na.ssl-images-amazon.com/images/I/71s9FMKzr%2BL._SL1500_.jpg',
         name: 'Headphones',
-        subTitle: 'Sony',
-        color: 'Red',
         price: '200',
+        quantity: '200',
       },
       {
-        src:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTM9C9aP58i8tP7Es4wi9Jp2ZQDsHowg3c0pw&usqp=CAU',
-        name: 'iPad Pro',
-        subTitle:
-          'It’s a magical piece of glass.It’s so fast most PC laptops can’t catch up. It has pro cameras that can transform reality. And you can use it with touch, pencil, keyboard and now trackpad.It’s the new iPad Pro.',
-        color: 'Gold',
-        price: '600',
-      },
+        image_link:
+          'https://images-na.ssl-images-amazon.com/images/I/71s9FMKzr%2BL._SL1500_.jpg',
+        name: 'Headphones',
+        price: '200',
+        quantity: '200',
+      }
     ]
   },
 ];
@@ -54,13 +58,37 @@ export default function ShopSearch(props) {
   return products ? (
     <LandingPage>
       <Grid>
+    <TableContainer>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell></TableCell>
+                        <TableCell>Product</TableCell>
+                        <TableCell>Shop</TableCell>
+                        <TableCell>Price</TableCell>
+                    </TableRow>
+                </TableHead>
+            </Table>
+        </TableContainer>
+    
+
+    
+      
         {products.map((product) => (
-          <Grid item key={product.id} >
+          
+          return {
+            <>
+            {product.products.map((prod)=>(
+            <Grid item key={product.id} >
             <ProductCard
               product={product}
             />
           </Grid>
-        ))}
+          ))}
+            </>
+            }
+          
+          ))}
       </Grid>
     </LandingPage>
 
